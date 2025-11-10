f0e0df9de5e835504d805eecccd64fc8087d1456
Skyscreen <saladecroutons@gmail.com>
2025-11-10 13:46:23 -0800

    Rename creep spawning handler

---
+++ handler.spawning.js
@@ -0,0 +1,14 @@
+// Handles the spawning of different creeps
+/** 
+ * @param {string} roomName
+**/
+function spawnCreeps(roomName) {
+    // Get spawns in room from memory
+    var roomSpawns = Memory.rooms[roomName].spawns;
+    
+    
+}
+
+exports.module = {
+    spawnCreeps
+}
--- handler.spawns.js
+++
@@ -1,9 +0,0 @@
-// Handles the spawning of different creeps
-var handler_Spawns = {
-
-    run: function() {
-        
-    }
-};
-
-exports.module = handler_Spawns;
