b488a1058d62fe3f0b50d5973fac496e68e33142
Skyscreen <saladecroutons@gmail.com>
2025-11-10 15:18:04 -0800

    Update colors

--- handler.logging.js
+++ handler.logging.js
@@ -3,8 +3,8 @@ const LOG_TYPE = {
     WARNING: '#e65c00',
     REGULAR: '#c7c7c7',
     TRIVIAL: '#999999',
-    INFORMATION: '#809fff',
-    SUCCESS: '#93ff80ff',
+    INFORMATION: '#7b9bfdff',
+    SUCCESS: '#33ff0041',
     ALERT: '#ffff00'
 }
 
