401dbac3e94e5baf48f9558d65ef11d9e66b0f05
Silas Walker-Tjepkes <saladecroutons@gmail.com>
2025-11-12 02:55:44 -0800

    Begin writing spawning handler

--- handler.spawning.js
+++ handler.spawning.js
@@ -1,10 +1,15 @@
-// Handles the spawning of different creeps
+// Handles the spawning of different creeps based on the creep queue
 /** 
  * @param {string} roomName
 **/
 function spawnCreeps(roomName) {
     // Get spawns in room from memory
     var roomSpawns = Memory.rooms[roomName].spawns;
+
+    // Each spawn can produce a single creep
+    for (let spawn in roomSpawns) {
+
+    }
     
     
 }
